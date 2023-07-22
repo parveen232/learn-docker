f58504a6bffe0d228acad50892753d50dc1f46a4
parveen232 <parveen297m210@gmail.com>
2023-07-22 13:20:49 +0530

    build image

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.use((req, res) => {
+    console.log("we got a new request");
+    res.send("<h1>This is a WebPage!</h1>")
+})
+
+app.listen(3000, () => {
+    console.log("listening on port 3000")
+})
